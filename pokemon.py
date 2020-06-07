4ff9322444d00571ef8fd60560bd7ed99073e4ae
EAFD88 <edd.danon@gmail.com>
2020-06-07 16:18:48 +0100

    Initial commit, pokemon class init created

---
+++ pokemon.py
@@ -0,0 +1,10 @@
+# codecademy Pokemon project
+
+class Pokemon:
+	#name=str, element=str, max_health=int, current_health=int, faint=bool
+	__init__(self, name, element, max_health, current_health, faint):
+	self.name=name
+	self.element=element
+	self.max_health=max_health
+	self.current_health=current_health
+	self.faint=faint
